4765bcb0929055e3c44e5c1f8e66fb6deadb5f8e
ltoshchev <ltoshchev@yandex-team.ru>
2020-05-09 22:27:57 +0300

    - wrote second article - update layout a little - update version of ctchi

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate ctchi;
 
-use ctchi::core::ctchi::{Ctchi, Config, Routes};
+use ctchi::core::ctchi::{Ctchi, Config};
+use ctchi::core::routes::Routes;
 
 fn main() {
     let mut routes = Routes::new();
@@ -8,13 +9,14 @@ fn main() {
     routes.add_route("/favicon.ico", "src/static/404.html");
     routes.add_route("/blog", "src/static/blog.html");
     routes.add_route("/blog/1", "src/static/blog/first_day.html");
+    routes.add_route("/blog/2", "src/static/blog/second_day.html");
     routes.add_route("/404", "src/static/404.html");
 
     let configuration = Config {
-//        bind_path: "138.197.210.225:80",
-        bind_path: "127.0.0.1:8080",
-        static_path: "/home/ltoshchev/programming/rust/glotitude_hq/",
-//        static_path: "/var/www/gl/",
+        bind_path: "138.197.210.225:80",
+//        bind_path: "127.0.0.1:8080",
+//        base_path: "/home/ltoshchev/programming/rust/glotitude_hq/",
+        base_path: "/var/www/gl/",
         routes,
     };
 
