118c05c4df9c130ada3332911dc20ace58cd439e
ltoshchev <ltoshchev@yandex-team.ru>
2020-05-11 20:09:26 +0300

    - add article about third day - increment ctchi version - move styles into css file

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ use ctchi::core::routes::Routes;
 
 fn main() {
     let mut routes = Routes::new();
-    routes.add_route("/", "src/static/index.html");
-    routes.add_route("/favicon.ico", "src/static/404.html");
-    routes.add_route("/blog", "src/static/blog.html");
-    routes.add_route("/blog/1", "src/static/blog/first_day.html");
-    routes.add_route("/blog/2", "src/static/blog/second_day.html");
-    routes.add_route("/404", "src/static/404.html");
+    routes.add_route("/", "static/index.html");
+    routes.add_route("/favicon.ico", "static/404.html");
+    routes.add_route("/blog", "static/blog.html");
+    routes.add_route("/blog/1", "static/blog/first_day.html");
+    routes.add_route("/blog/2", "static/blog/second_day.html");
+    routes.add_route("/blog/3", "static/blog/third_day.html");
+    routes.add_route("/404", "static/404.html");
 
     let configuration = Config {
         bind_path: "138.197.210.225:80",
 //        bind_path: "127.0.0.1:8080",
-//        base_path: "/home/ltoshchev/programming/rust/glotitude_hq/",
-        base_path: "/var/www/gl/",
+//        base_path: "/home/ltoshchev/programming/rust/glotitude_hq/src/",
+        static_uri_pref: "/static/",
+        base_path: "/var/www/gl/src/",
         routes,
     };
 
